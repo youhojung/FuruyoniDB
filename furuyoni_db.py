faf84b84505fb95d62dff42096d98e26bb1b2fe7
youhojung <74446224+youhojung@users.noreply.github.com>
2024-12-15 09:53:02 +0900

    DB 업데이트 및 html 업데이트

--- furuyoni_db.py
+++ furuyoni_db.py
@@ -13,13 +13,32 @@ def cardDBmenu():
     db = sqlite3.connect("furuyoni_project.db")
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
-    items = cursor.execute('SELECT DISTINCT cardchara FROM CardInfo').fetchall()
+    items = cursor.execute('SELECT Kname, charaname, another, cNum FROM CharaInfo ORDER BY cNum').fetchall()
     db.close()
     return render_template('cardDBmenu.html', chara=items)
 
-@app.route('/furuDB/cardDB/<string:chara_id>/')
-def chara_card(chara_id):
-    return 'asdf'
+@app.route('/furuDB/cardDB/<string:charaname>-<string:another>/')
+def cardInfo(charaname, another):
+    db = sqlite3.connect("furuyoni_project.db")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    chara_info = cursor.execute(
+        'SELECT * FROM CharaInfo WHERE charaname = ? AND another = ?',
+        (charaname, another)
+    ).fetchone()
+    card_data = cursor.execute(
+        'SELECT * FROM CardInfo WHERE cardchara = ? AND cardID Like ?',
+        (charaname, f'%-{another}-%')
+    ).fetchall()
+    db.close()
+    return render_template(
+        'cardInfo.html',
+        charaname=charaname,
+        another=another,
+        chara_info=chara_info,
+        card_data=card_data
+    )
+
 
 @app.route('/furuDB/matchDB/')
 def matchDBmenu():
